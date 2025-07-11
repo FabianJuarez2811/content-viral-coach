3a6c6807d7df99dd9b117f5385f8703759d570d4
Fabian Juarez <fabian.juarez2801@gmail.com>
2025-07-11 00:54:06 -0600

    fix: solo un export default en Home

--- app/page.tsx
+++ app/page.tsx
@@ -14,19 +14,19 @@ import {
   getUser
 } from '@/utils/supabase/queries';
 
-export default async function PricingPage() {
-  const supabase = createClient();
-  const [user, products, subscription] = await Promise.all([
-    getUser(supabase),
-    getProducts(supabase),
-    getSubscription(supabase)
-  ]);
+//export default async function PricingPage() {
+  //const supabase = createClient();
+  //const [user, products, subscription] = await Promise.all([
+    //getUser(supabase),
+    //getProducts(supabase),
+    //getSubscription(supabase)
+  //]);
 
-  return (
-    <Pricing
-      user={user}
-      products={products ?? []}
-      subscription={subscription}
-    />
-  );
-}
+ // return (
+  //  <Pricing
+    //  user={user}
+      //products={products ?? []}
+      //subscription={subscription}
+    ///>
+  //);
+//}
